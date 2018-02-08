9fbc5892fd4ecb6c7c2c6d6bea0cbc399eb0cc80
Samantha Herzog <samiherzog@0587363746.wireless.umich.net>
2018-02-08 09:39:36 -0500

    hello

---
+++ scrapehello.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+
+f= open('hello.html')
+html= f.read()
+soup= BeautifulSoup(html, 'html.parser')
+
+print(soup.prettify()) #prints to consule our html file we loaded (it's a string)
+
+# searching by tag
+all_list_items = soup.find_all('li')
+all_divs = soup.find_all('div')
+
+# searching by class
+all_goodbye_elements = soup.find_all(class_='goodbye') #notice class_
+
+# searching by tag AND class
+all_french_list_items = soup.find_all('li', class_='french')
+
+# searching by id
+all_hello_elements = soup.find_all(id='hello-list')
+
+
+print('list items:', all_list_items)
+print('------')
+print('divs:', all_divs)
+print('------')
+print('goodbye elements:', all_goodbye_elements)
+print('------')
+print('french stuff:', all_french_list_items)
+print('------')
+print('hello id elements:', all_hello_elements)
+print('------')
+
+
+print(type(all_list_items[0])) #returns: <class 'bs4.element.Tag'>
+print('------')
+
+
+for c in all_list_items:
+  print(c.string)
+print('------')
+
+print('Children of hello-list:')
+for c in all_hello_elements[0].children:
+  print(c.string)
+print('------')
+
+
+print('List items within the hello tag')
+hello_list_items = all_hello_elements[0].find_all('li')
+print (hello_list_items)
+print('------')
+
+
+all_hello_elements = soup.find_all(id='hello-list')
+print(all_hello_elements[0])
+print('------')
+#above and below DO THE SAME THING!
+#‘find( )’ always just returns the first matching element
+#so only use it when you know there’s only one (or you know you only want the first one).
+the_hello_element = soup.find(id='hello-list')
+print(the_hello_element)
+print('------')
+
+
+img_tag = soup.find('img')
+print('The img source:')
+print(img_tag['src'])
+print('------')
